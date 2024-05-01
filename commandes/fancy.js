edc41a77fa90b7319dc10b01575f905b01b35613
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-05-01 01:39:33 +0000

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 zokou-md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('Zokou-md', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 Hacking-md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('Hacking-md', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
