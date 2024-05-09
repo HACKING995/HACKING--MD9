73cee3d091527440ffff0812a7be70797006c4f8
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-05-09 11:51:43 +0000

    Update youtube.js

--- commandes/youtube.js
+++ commandes/youtube.js
@@ -7,7 +7,7 @@ const ffmpeg = require("fluent-ffmpeg");
 const yts1 = require("youtube-yts");
 //var fs =require("fs-extra")
 
-zokou({nomCom: "play", "song",
+zokou({nomCom: "play", 
 categorie: "Recherche",
 reaction: "💿"
 }, async (origineMessage, zk, commandeOptions) => {
