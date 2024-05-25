59c33f78ca8e0d67d7f6e5fde40aeb57b3696c46
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-05-25 15:13:13 +0000

    𝐇𝐀𝐂𝐊𝐈𝐍𝐆-𝐌𝐃

--- commandes/hack.js
+++ commandes/hack.js
@@ -8,81 +8,62 @@ zokou({ nomCom: 'hack',
     reaction: '🦠', 
     fromMe: 'true', 
 
-       
   },
   async (dest, zk, commandeOptions) => {
     const { repondre, arg, ms } = commandeOptions;
 
     await zk.sendMessage(dest, "```thomas-md Injecting malware```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "```hacking into device \n 0%```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "```transfering photos \n █ 10%```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "```transfer successful \n █ █ 20%```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "```transfering videos \n █ █ █ 30%```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "```transfer successful \n █ █ █ █ 40%```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "```transfering audio \n █ █ █ █ █ 50%```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "```transfer successful \n █ █ █ █ █ █ 60%```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "```transfering hidden files \n █ █ █ █ █ █ █ 70%```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "```transfer successful \n █ █ █ █ █ █ █ █ 80%```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "```transfering whatsapp chat \n █ █ █ █ █ █ █ █ █ 90%```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "```transfer successful \n █ █ █ █ █ █ █ █ █ █ 100%```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "```System hyjacking on process.. \n Conecting to Server ```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "```Divice successfully connected... \n Riciving data...```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "```Data hyjacked from divice 100% completed \n killing all evidence killing all malwares...```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "``` HACKING COMPLETED ```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "``` SENDING PHONE DOCUMENTS...```");
-
     await sleep(30000);
 
     await zk.sendMessage(dest, "``` SUCCESSFULLY SENT DATA AND Connection disconnected```");
-
     await sleep(30000);
 
     return zk.sendMessage(dest, '*ALL FILES TRANSFERRED*');
@@ -91,4 +72,3 @@ zokou({ nomCom: 'hack',
 async function sleep(ms) {
   await new Promise(resolve => setTimeout(resolve, ms));
 }
-
