35bd047d5c7d369ed5f3b116c3235fa2d1083cdf
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-07-25 01:46:46 +0000

    test new

--- commandes/GPT.js
+++ commandes/GPT.js
@@ -3,55 +3,219 @@ const traduire = require("../framework/traduction");
 const s = require('../set');
 const axios = require('axios');
 
+// Flash command
 zokou({
-  nomCom: "gpt4",
-  reaction: "📡",
-  categorie: "IA"
-}, async (dest, zk, commandeOptions) => {
-  const { repondre, ms, arg } = commandeOptions;
-
-  async function gpt4(prompt) {
-    const headers = {
-      'Content-Type': 'application/json',
-      'Referer': 'https://chatgpt4online.org/',
-      'Sec-Ch-Ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-      'Sec-Ch-Ua-Mobile': '?0',
-      'Sec-Ch-Ua-Platform': '"Windows"',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-      'X-Wp-Nonce': '152990aad3'
-    };
-
-    const params = {
-      "botId": "default",
-      "customId": null,
-      "session": "N/A",
-      "chatId": "r20gbr387ua",
-      "contextId": 58,
-      "messages": [
-        {
-          "id": "0aqernpzbas7",
-          "role": "assistant",
-          "content": "Hi! How can I help you?",
-          "who": "AI: ",
-          "timestamp": 1719360952775
-        }
-      ],
-      "newMessage": prompt,
-      "newFileId": null,
-      "stream": false
-    };
-
-    try {
-      const response = await axios.post("https://chatgpt4online.org/wp-json/mwai-ui/v1/chats/submit", params, { headers });
-      console.log('Response:', response.data);
-      return response.data;
-    } catch (error) {
-      console.error('Error:', error);
-      throw error;
-    }
+  'nomCom': "alexa",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (message, client, options) => {
+  const { repondre, ms, arg } = options;
+  
+  if (!arg || !arg[0]) {
+    return repondre("YEES!\n _I'm listening to you._");
   }
-
-  const userPrompt = arg || 'Kapan kamu di-update??';
-  const response = await gpt4(userPrompt);
-  repondre(response);
+  
+  try {
+    const userInput = arg.join(" ");
+    const response = await fetch(`http://api.brainshop.ai/get?bid=181821&key=ltFzFIXrtj2SVMTX&uid=[uid]&msg=${userInput}`);
+    const data = await response.json();
+    await repondre(data.cnt);
+  } catch {
+    repondre("Something went wrong...");
+  }
 });
+
+// Dalle command
+zokou({
+  'nomCom': 'dalle2',
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (message, client, options) => {
+  const { repondre, arg, ms } = options;
+  
+  try {
+    if (!arg || arg.length === 0) {
+      return repondre("Please enter the necessary information to generate the image.");
+    }
+    
+    const prompt = arg.join(" ");
+    const imageUrl = `https://cute-tan-gorilla-yoke.cyclic.app/imagine?text=${prompt}`;
+    
+    client.sendMessage(message, {
+      'image': { 'url': imageUrl },
+      'caption': "*powered by FLASH-MD*"
+    }, { 'quoted': ms });
+  } catch (error) {
+    console.error('Error:', error.message || "An error occurred");
+    repondre("Oops, an error occurred while processing your request");
+  }
+});
+
+// GPT command
+zokou({
+  'nomCom': 'gpt3.5',
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (message, client, options) => {
+  const { repondre, arg, ms } = options;
+  
+  if (!arg || arg.length === 0) {
+    return repondre("Please ask a question.");
+  }
+  
+  const prompt = arg.join(" ");
+  const response = await fetch(`https://api.maher-zubair.tech/ai/chatgpt3?q=${prompt}`);
+  const data = await response.json();
+  await repondre(data.result);
+  console.log(data.completion);
+});
+
+// Gemini command
+zokou({
+  'nomCom': "gemini",
+  'reaction': '🤗',
+  'categorie': 'AI'
+}, async (message, client, options) => {
+  const { repondre, arg, ms } = options;
+  
+  if (!arg || arg.length === 0) {
+    return repondre("Hello, I'm *FLASH-MD*, an AI developed by France King.\n\nWhat help can I offer you today?");
+  }
+  
+  const prompt = arg.join(" ");
+  const response = await fetch(`https://api.maher-zubair.tech/ai/chatgpt3?q=${prompt}`);
+  const data = await response.json();
+  await repondre(data.result);
+  console.log(data.completion);
+});
+
+// Calculator command
+zokou({
+  'nomCom': 'calcul2',
+  'reaction': '🔢',
+  'categorie': 'General'
+}, async (message, client, options) => {
+  const { repondre, arg, ms } = options;
+  
+  if (!arg || arg.length === 0) {
+    return repondre("Please insert math calculations like 100000+2024.\n\nNOTE: Use \"(/)\" for division and \"(*)\" for multiplication or letter x");
+  }
+  
+  const calculation = arg.join(" ");
+  const response = await fetch(`https://api.maher-zubair.tech/ai/mathssolve?q=${calculation}`);
+  const data = await response.json();
+  await repondre(data.result);
+  console.log(data.completion);
+});
+
+// GPT-4 command
+zokou({
+  'nomCom': "gpt4",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (message, client, options) => {
+  const { repondre, arg, ms } = options;
+  
+  try {
+    if (!arg || arg.length === 0) {
+      return repondre("Please ask a question.");
+    }
+    
+    const prompt = arg.join(" ");
+    const response = await axios.get(`https://api.maher-zubair.tech/ai/chatgpt4?q=${prompt}`);
+    const data = response.data;
+    
+    if (data) {
+      repondre(data.data);
+    } else {
+      repondre("Error during response generation.");
+    }
+  } catch (error) {
+    console.error('Error:', error.message || "An error occurred");
+    repondre("Oops, an error occurred while processing your request.");
+  }
+});
+
+// Best Wallpaper command
+zokou({
+  'nomCom': "best-wallp",
+  'reaction': '🙌',
+  'categorie': "Thomas"
+}, async (message, client, options) => {
+  const { repondre, arg, ms } = options;
+  
+  const response = await fetch("https://api.unsplash.com/photos/random?client_id=72utkjatCBC-PDcx7-Kcvgod7-QOFAm2fXwEeW8b8cc");
+  const data = await response.json();
+  const imageUrl = data.urls.regular;
+  
+  let imageMessage = {
+    'image': { 'url': imageUrl },
+    'caption': "*POWERED BY FLASH-MD*"
+  };
+  
+  return await client.sendMessage(message, imageMessage, { 'quoted': ms });
+});
+
+// Random Image command
+zokou({
+  'nomCom': "random",
+  'reaction': '🥂',
+  'categorie': "Thomas"
+}, async (message, client, options) => {
+  const { repondre, arg, ms } = options;
+  
+  const response = await fetch("https://api.unsplash.com/photos/random?client_id=72utkjatCBC-PDcx7-Kcvgod7-QOFAm2fXwEeW8b8cc");
+  const data = await response.json();
+  const imageUrl = data.urls.regular;
+  
+  let imageMessage = {
+    'image': { 'url': imageUrl },
+    'caption': "*POWERED BY FLASH-MD*"
+  };
+  
+  return await client.sendMessage(message, imageMessage, { 'quoted': ms });
+});
+
+// Nature Image command
+zokou({
+  'nomCom': "nature",
+  'reaction': '🦗',
+  'categorie': "Thomas"
+}, async (message, client, options) => {
+  const { repondre, arg, ms } = options;
+  
+  const response = await fetch("https://api.unsplash.com/photos/random?client_id=72utkjatCBC-PDcx7-Kcvgod7-QOFAm2fXwEeW8b8cc");
+  const data = await response.json();
+  const imageUrl = data.urls.regular;
+  
+  let imageMessage = {
+    'image': { 'url': imageUrl },
+    'caption': "*POWERED BY FLASH-MD*"
+  };
+  
+  return await client.sendMessage(message, imageMessage, { 'quoted': ms });
+});
+
+// Time command
+zokou({
+  'nomCom': 'time',
+  'reaction': '⌚',
+  'categorie': "General"
+}, async (message, client, options) => {
+  const { repondre, arg, ms } = options;
+  
+  try {
+    if (!arg || arg.length === 0) {
+      return repondre("Enter the name of the country you want to know its time and date");
+    }
+    
+    const country = arg.join(" ");
+    const response = await fetch(`https://levanter.onrender.com/time?code=${country}`);
+    const data = await response.json();
+    const time = data.result[0].time;
+    await repondre(time);
+  } catch (error) {
+    repondre("That country name is incorrect!");
+  }
+});
+```
