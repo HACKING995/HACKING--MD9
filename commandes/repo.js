043cd14df14c462a52e70c19581e8634b9bb1cde
THOMAS DEVELOPPEUR <154031706+HACKING995@users.noreply.github.com>
2025-02-17 18:34:33 +0000

    Update and rename repo.json to repo.js

--- commandes/repo.js
+++ commandes/repo.js
@@ -1,4 +1,4 @@
-const { zokou } = require("../framework/zokou");
+/*const { zokou } = require("../framework/zokou");
 const { getytlink, ytdwn } = require("../framework/ytdl-core");
 const yts = require("yt-search");
 const BaseUrl = 'https://api.giftedtech.my.id';
