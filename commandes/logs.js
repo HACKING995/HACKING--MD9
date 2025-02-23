476bc47a502f892e771dc44b39b22ef0eae844b5
THOMAS DEVELOPPEUR <154031706+HACKING995@users.noreply.github.com>
2025-02-23 00:02:07 +0000

    Update logs.js

--- commandes/logs.js
+++ commandes/logs.js
@@ -75,24 +75,24 @@ function addTextproCommand(nomCom, text_pro_url, type) {
 }
 
 // Ajout des commandes
-addTextproCommand("dragonball1", "https://en.ephoto360.com/create-dragon-ball-style-text-effects-online-809.html", 1);
+addTextproCommand("dragonball", "https://en.ephoto360.com/create-dragon-ball-style-text-effects-online-809.html", 1);
 addTextproCommand("deadpool1", "https://en.ephoto360.com/create-text-effects-in-the-style-of-the-deadpool-logo-818.html", 2);
 addTextproCommand("blackpink", "https://en.ephoto360.com/create-a-blackpink-style-logo-with-members-signatures-810.html", 1);
-addTextproCommand("neon1", "https://en.ephoto360.com/blue-neon-text-effect-117.html", 1);
+addTextproCommand("neon", "https://en.ephoto360.com/blue-neon-text-effect-117.html", 1);
 addTextproCommand("football", "https://en.ephoto360.com/paul-scholes-shirt-foot-ball-335.html", 2);
 addTextproCommand("steel", "https://en.ephoto360.com/heated-steel-lettering-effect-65.html", 2);
 addTextproCommand("paint", "https://en.ephoto360.com/paint-splatter-text-effect-72.html", 1);
-addTextproCommand("thunder1", "https://en.ephoto360.com/thunder-text-effect-online-97.html", 1);
+addTextproCommand("thunder", "https://en.ephoto360.com/thunder-text-effect-online-97.html", 1);
 addTextproCommand("thor", "https://en.ephoto360.com/create-thor-logo-style-text-effects-online-for-free-796.html", 1);
 addTextproCommand("graffiti1", "https://en.ephoto360.com/cute-girl-painting-graffiti-text-effect-667.html", 2);
-addTextproCommand("gold2", "https://en.ephoto360.com/modern-gold-5-215.html", 1);
+addTextproCommand("gold", "https://en.ephoto360.com/modern-gold-5-215.html", 1);
 addTextproCommand("neon2", "https://en.ephoto360.com/create-light-effects-green-neon-online-429.html", 1);
 addTextproCommand("effacer", "https://en.ephoto360.com/create-eraser-deleting-text-effect-online-717.html", 1);
 addTextproCommand("galaxy", "https://en.ephoto360.com/text-light-galaxy-effectt-345.html", 1);
 addTextproCommand("vintage", "https://en.ephoto360.com/write-text-on-vintage-television-online-670.html", 1);
 addTextproCommand("gold1", "https://en.ephoto360.com/gold-text-effect-158.html", 1);
 addTextproCommand("graffiti2", "https://en.ephoto360.com/graffiti-text-5-180.html", 1);
-addTextproCommand("hacker1", "https://en.ephoto360.com/create-anonymous-hacker-avatars-cyan-neon-677.html", 1);
+addTextproCommand("hacker", "https://en.ephoto360.com/create-anonymous-hacker-avatars-cyan-neon-677.html", 1);
 addTextproCommand("rain", "https://en.ephoto360.com/foggy-rainy-text-effect-75.html", 1);
 addTextproCommand("typography", "https://en.ephoto360.com/create-online-typography-art-effects-with-multiple-layers-811.html", 1);
 addTextproCommand("gold3", "https://en.ephoto360.com/glossy-chrome-text-effect-online-424.html", 1);
@@ -100,12 +100,12 @@ addTextproCommand("wood", "https://en.ephoto360.com/create-3d-wood-text-effects-
 addTextproCommand("captain_america", "https://en.ephoto360.com/create-a-cinematic-captain-america-text-effect-online-715.html", 2);
 addTextproCommand("cubic", "https://en.ephoto360.com/3d-cubic-text-effect-online-88.html", 1);
 addTextproCommand("green_effect", "https://en.ephoto360.com/create-unique-word-green-light-63.html", 1);
-addTextproCommand("naruto1", "https://en.ephoto360.com/naruto-shippuden-logo-style-text-effect-online-808.html", 1);
+addTextproCommand("naruto", "https://en.ephoto360.com/naruto-shippuden-logo-style-text-effect-online-808.html", 1);
 addTextproCommand("sand", "https://en.ephoto360.com/realistic-3d-sand-text-effect-online-580.html", 1);
 addTextproCommand("plasma", "https://en.ephoto360.com/plasma-text-effects-online-71.html", 1);
-addTextproCommand("avengers1", "https://en.ephoto360.com/create-logo-3d-style-avengers-online-427.html", 2);
+addTextproCommand("avengers", "https://en.ephoto360.com/create-logo-3d-style-avengers-online-427.html", 2);
 addTextproCommand("underwater", "https://en.ephoto360.com/3d-underwater-text-effect-online-682.html", 1);
-addTextproCommand("glass1", "https://en.ephoto360.com/write-text-on-wet-glass-online-589.html", 1);
+addTextproCommand("glass", "https://en.ephoto360.com/write-text-on-wet-glass-online-589.html", 1);
 addTextproCommand("graffiti3", "https://en.ephoto360.com/cover-graffiti-181.html", 1);
 addTextproCommand("summery", "https://en.ephoto360.com/create-a-summery-sand-writing-text-effect-577.html", 1);
 addTextproCommand("gold4", "https://en.ephoto360.com/modern-gold-silver-210.html", 1);
